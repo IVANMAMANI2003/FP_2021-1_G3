7c100782c627650d90be20bc9983d8585408dec5
IVANMAMANI2003 <>
2021-04-28 21:37:30 +0000

    un ejercicio de condicional

--- EstCondicional.java
+++ EstCondicional.java
@@ -18,6 +18,25 @@ class EstCondicional{
     //Datos de salida
     System.out.println("El monto a pagar es:"+montoP);
    } 
-    public static void main(String[] arg){ ejercicio01();
+   static void ejercicio02(){ 
+     //Definir variables y otros 
+     System.out.println("Ejemplo estructura Condicional 02 en Java"); 
+     int cantidadX=0; 
+     double montoP=0; 
+     //Datos de Entrada 
+     System.out.println("Ingrese la cantidad de personas:"); 
+     cantidadX=teclado.nextInt(); 
+     //Proceso 
+     if(cantidadX<=200){ 
+       montoP=cantidadX*95; 
+     }else if(cantidadX>200 && cantidadX<=300){ montoP=cantidadX*85; 
+     }else{ 
+       montoP=cantidadX*75; } 
+      //Datos de salida 
+     System.out.println("El monto a pagar es:"+montoP); 
+    }
+    public static void main(String[] arg){ 
+    //ejercicio01();
+    ejercicio02();
   } 
 }
--- EstCondicional.py
+++ EstCondicional.py
@@ -11,4 +11,21 @@ def EstCondicional01():
     montoP=cantidadX*0.90
   #Datos de salida
   print("El monto a pagar es:", montoP)
-EstCondicional01()
+#EstCondicional01()
+
+def EstCondicional02():
+  #Definir variables y otros
+  print("Ejercicio 02 Est. Condicional")
+  montoP=0
+  #Datos de entrada
+  cantidadX=int(input("Ingrese la cantidad personas invitadas:"))
+  #Proceso
+  if cantidadX<200:
+    montoP=cantidadX*95
+  elif cantidadX>200 and cantidadX<=300:
+    montoP=cantidadX*85
+  else:
+    montoP=cantidadX*75
+  #Datos de salida
+  print("La cantidad a pagar es:", montoP)
+EstCondicional02()
