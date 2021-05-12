8938d3f44cac6c5d50dbb57c4d862a764d418212
IVANMAMANI2003 <ivanyomm.2003@gmail.com>
2021-05-12 14:43:10 -0500

    creando nueva carpeta de estructura repititivo

---
+++ structure-repititive/Prueba.java
@@ -0,0 +1,10 @@
+public class Prueba {
+    public static void probando() {
+        System.out.println("probando si funciona");
+}
+  public static void main(String[] args) {
+      System.out.println("holassssss");
+      probando();
+  }
+
+}
